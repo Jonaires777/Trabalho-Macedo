2bfb8c191caab8528b7bca905670e35b14e4b7f5
alanz <alanzcc1@gmail.com>
2023-06-22 08:13:21 -0300

    adicionadas strings

---
+++ spring/spring/src/main/java/project/spring/SongModel.java
@@ -0,0 +1,20 @@
+public class songModel() {
+
+    String title;
+    String artist;
+    String album;
+    String genre;
+    String songID;
+    String albumID;
+    String artistID;
+    String genreID;
+
+    this.title = title;
+    this.artist = artist;
+    this.album = album;
+    this.genre = genre;
+    this.songID = songID;
+    this.albumID = albumID;
+    this.artistID = artistID;
+    this.genreID = genreID;
+}
